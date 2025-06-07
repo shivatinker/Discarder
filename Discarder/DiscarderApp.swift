153560884d1985747f316d0a2de40238c841b7e3
Andrii Zinoviev <azinoviev@readdle.com>
2025-06-07 16:27:44 +0200

    Initial Commit

---
+++ Discarder/DiscarderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscarderApp.swift
+//  Discarder
+//
+//  Created by Andrii Zinoviev on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct DiscarderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
